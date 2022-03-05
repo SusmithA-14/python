bcf84b4e5f7eee4a640a3598fa7f23b411586137
Susmitha Dannana <84056805+SusmithA-14@users.noreply.github.com>
2022-03-05 19:20:11 +0530

    Add files via upload

---
+++ search.py
@@ -0,0 +1,27 @@
+#search Engine
+
+"""You’re working on a search engine. Watch your back Google!
+
+The given code takes a text and a word as input and passes them to a function called search().
+
+The search() function should return "Word found" if the word is present in the text, or "Word not found", if it’s not.
+
+Sample Input
+"This is awesome"
+"awesome"
+
+Sample Output
+Word found
+"""
+#code:
+
+text = input()
+word = input()
+def search(text,word):
+    text=text.split(' ')
+    if word in text:
+        return("Word found")
+    else:
+        return("Word not found")
+
+print(search(text, word))
